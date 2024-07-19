78a1a52010952fec836672d3f6487b83a1b00926
rodgerswisdom <trulyhawona@gmail.com>
2024-07-19 13:40:41 +0300

    Implement Routing

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,8 +1,16 @@
 <?php
 
 use CodeIgniter\Router\RouteCollection;
+use App\Controllers\Pages;
 
 /**
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+/**
+ * 'Home::index' - any incoming unspecified content should be directed to index() method in Home controller
+ */
+
+ $routes->get('pages', [Pages::class, 'index']);
+ $routes->get('(:segment)', [Pages::class, 'view']);
