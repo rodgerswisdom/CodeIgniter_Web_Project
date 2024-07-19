3a1fe1586cf836bd60f1f1eaeae21a118385d13c
rodgerswisdom <trulyhawona@gmail.com>
2024-07-19 14:02:29 +0300

    feat: Pages::view()

--- app/Controllers/Pages.php
+++ app/Controllers/Pages.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Controllers;
+use CodeIgniter\Exceptions\PageNotFoundException;
 
 class Pages extends BaseController
 {
@@ -12,5 +13,16 @@ class Pages extends BaseController
     public function view(string $page = 'home')
     {
         // ...
+        if (! is_file(APPPATH . 'Views/pages/' . $page . '.php')) {
+            // Whoops, we don't have a page for that!
+            throw new PageNotFoundException($page);
+        }
+
+        $data['title'] = ucfirst($page); // Capitalize the first letter
+
+        return view('templates/header', $data)
+            . view('pages/' . $page)
+            . view('templates/footer');
+
     }
 }
